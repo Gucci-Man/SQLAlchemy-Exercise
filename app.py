645573f673cc2e412237c58084e5c3dae3cbf130
Adel <adel.ngo@yahoo.com>
2023-09-07 23:00:26 -0500

    Added details page

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ app.app_context().push()
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///blogly"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_ECHO"] = True
+app.config["SECRET_KEY"] = "IAMBATMAN"
 app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
 debug = DebugToolbarExtension(app)
 
@@ -19,7 +20,26 @@ db.create_all()
 
 
 @app.route("/")
+def home():
+    """Redirects to list of users"""
+    return redirect("/users")
+
+
+@app.route("/users")
 def list_users():
     """Shows list of all users in db"""
     users = User.query.all()
-    return render_template(list.html, users=users)
+    return render_template("list.html", users=users)
+
+
+@app.route("/users/new")
+def new_user():
+    """Show an add form for users"""
+    return render_template("form.html")
+
+
+@app.route("/<int:user_id>")
+def show_user(user_id):
+    """Show details about a user"""
+    user = User.query.get_or_404(user_id)
+    return render_template("details.html", user=user)
