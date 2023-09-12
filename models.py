c47a96a855fd72a1a8e0d714cc36b2f5514fb628
Adel <adel.ngo@yahoo.com>
2023-09-11 20:52:06 -0500

    Added Post model

--- models.py
+++ models.py
@@ -1,7 +1,8 @@
 """Models for Blogly."""
 
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import text
+from sqlalchemy import text, DateTime, func
+import datetime
 
 # initialize SQLAlchemy
 db = SQLAlchemy()
@@ -17,7 +18,7 @@ def connect_db(app):
 class User(db.Model):
     __tablename__ = "users"
 
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
 
     first_name = db.Column(db.String(50), nullable=False)
 
@@ -28,3 +29,20 @@ class User(db.Model):
         nullable=True,
         default="https://img.freepik.com/premium-vector/people-icon-person-symbol-vector-illustration_276184-166.jpg?w=2000",
     )
+
+
+# Post Model
+class Post(db.Model):
+    __tablename__ = "posts"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+
+    title = db.Column(db.Text, nullable=False)
+
+    content = db.Column(db.Text, nullable=False)
+
+    created_at = db.Column(db.DateTime(timezone=True), default=func.now())
+
+    user_code = db.Column(db.Integer, db.ForeignKey("users.user_id"))
+
+    assignments = db.relationship("User", backref="posts")
